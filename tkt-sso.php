1167554a091a9dfa9e66307fce7484b8b5f029ea
Daniel Rockenstein <daniel.rockenstein@thekey.technology>
2021-09-13 11:47:12 +0200

    added update logic

--- tkt-sso.php
+++ tkt-sso.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TKT SSO
 Plugin URI:  https://www.thekey.technology
-Version:     1
+Version:     2
 Author:      the key technology
 Author URI:  https://www.thekey.technology
 License:     proprietary
@@ -21,6 +21,12 @@ if(! is_admin()) {
 }
 
 
-
+require 'plugin-update-checker-4.11/plugin-update-checker.php';
+$updateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/thekeytechnology/tk-sso.git',
+    __FILE__,
+    'tk-sso'
+);
+$updateChecker->setBranch('master');
 
 
