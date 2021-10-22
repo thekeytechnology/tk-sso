7ca9cf9ee2b49141c64057b25e7138a2f985ac53
Daniel Rockenstein <daniel.rockenstein@thekey.technology>
2021-10-22 16:36:27 +0200

    added new setting

--- admin/TkSsoSettingsPage.php
+++ admin/TkSsoSettingsPage.php
@@ -4,6 +4,7 @@ class TkSsoSettingsPage {
 
     public static $OPTION_SERVER_URL = "tkt_sso_server_url";
     public static $OPTION_LOGIN_URL = "tkt_sso_login_url";
+    public static $OPTION_ACCEPT_WORDPRESS_LOGIN = "tkt_sso_accept_wordpress_login";
 
     public function init() {
         add_action('admin_init', [$this, 'registerSettings']);
@@ -13,12 +14,12 @@ class TkSsoSettingsPage {
     public function registerSettings() {
         $this->registerSetting($this::$OPTION_SERVER_URL, "https://www.mypersonaltrainer.de/wp-content/themes/tkt/sso-server/api.php");
         $this->registerSetting($this::$OPTION_LOGIN_URL, "");
-
+        $this->registerSetting($this::$OPTION_ACCEPT_WORDPRESS_LOGIN, "0", ["description" => "0 = Nur der Login via Login Formular kann geschützte Inhalte freigeben. 1 = Wer im Wordpress angemeldet ist, wird behandelt, als wäre er via Drupal angemeldet."]);
     }
 
-    private function registerSetting($name, $default = '') {
+    private function registerSetting($name, $default = '', $args = []) {
         add_option($name, $default);
-        register_setting('tkt_sso_options_group', $name);
+        register_setting('tkt_sso_options_group', $name, $args);
     }
 
 
@@ -55,6 +56,18 @@ class TkSsoSettingsPage {
                             <input name="<?php echo $this::$OPTION_LOGIN_URL ?>" type="text"
                                    id="<?php echo $this::$OPTION_LOGIN_URL ?>"
                                    value="<?php echo get_option($this::$OPTION_LOGIN_URL); ?>"
+                                   class="regular-text code">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="<?php echo $this::$OPTION_ACCEPT_WORDPRESS_LOGIN ?>">Wordpress Login als SSO
+                                Login betrachten? 0 = Nein, 1 = Ja</label>
+                        </th>
+                        <td>
+                            <input name="<?php echo $this::$OPTION_ACCEPT_WORDPRESS_LOGIN ?>" type="text"
+                                   id="<?php echo $this::$OPTION_ACCEPT_WORDPRESS_LOGIN ?>"
+                                   value="<?php echo get_option($this::$OPTION_ACCEPT_WORDPRESS_LOGIN); ?>"
                                    class="regular-text code">
                         </td>
                     </tr>
