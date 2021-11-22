9e07bb325c725332688f8528a127b7557983e19e
danielrockenstein <daniel.rockenstein@thekey.technology>
2021-11-22 16:24:03 +0100

    bigfix

--- restrict-access/_restrict-access.php
+++ restrict-access/_restrict-access.php
@@ -5,5 +5,5 @@ require_once("elementor-add-on/_elementor-add-on.php");
 require_once("post-widget/_post-widgets.php");
 
 function tkSsoShouldRestrict() {
-    return !(is_admin() || current_user_can('editor') || current_user_can('administrator'));
+    return !(is_admin() || is_user_logged_in());
 }
--- tkt-sso.php
+++ tkt-sso.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TKT SSO
 Plugin URI:  https://www.thekey.technology
-Version:     15
+Version:     16
 Author:      the key technology
 Author URI:  https://www.thekey.technology
 License:     proprietary
