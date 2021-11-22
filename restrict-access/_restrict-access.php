c40f802d6f9132994a56aba97b971928962c0cc8
danielrockenstein <daniel.rockenstein@thekey.technology>
2021-11-22 16:22:58 +0100

    bigfix

--- restrict-access/_restrict-access.php
+++ restrict-access/_restrict-access.php
@@ -3,3 +3,7 @@
 require_once("TkSsoRoleManager.php");
 require_once("elementor-add-on/_elementor-add-on.php");
 require_once("post-widget/_post-widgets.php");
+
+function tkSsoShouldRestrict() {
+    return !(is_admin() || current_user_can('editor') || current_user_can('administrator'));
+}
