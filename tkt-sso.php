39082d729885dfc1679216bdaf183f10be9c4f62
danielrockenstein <daniel.rockenstein@thekey.technology>
2021-12-13 04:19:08 +0100

    bugfix

--- tkt-sso.php
+++ tkt-sso.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TKT SSO
 Plugin URI:  https://www.thekey.technology
-Version:     28
+Version:     29
 Author:      the key technology
 Author URI:  https://www.thekey.technology
 License:     proprietary
