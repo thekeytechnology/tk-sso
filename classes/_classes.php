4388f6f33b5a21accfa159ceb052629616c8b281
danielrockenstein <daniel.rockenstein@thekey.technology>
2022-02-24 23:44:08 +0100

    Bugfix

--- classes/_classes.php
+++ classes/_classes.php
@@ -5,6 +5,7 @@ require_once "TkDrupalSsoBroker.php";
 require_once "TkUsSsoBroker.php";
 require_once "TkSsoUser.php";
 require_once "TkSsoRoleManager.php";
+require_once "TkSsoUtil.php";
 
 global $tkSsoBroker;
 $tkSsoBroker = TkSsoUtil::getApiVersion() == "2" ? new TkUsSsoBroker() : new TkDrupalSsoBroker();
