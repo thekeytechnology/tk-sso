7e83e91e59f59b7f5d6a11fa88fe66bc265ee18c
danielrockenstein <daniel.rockenstein@thekey.technology>
2022-07-13 18:29:08 +0200

    Allow custom roles to be easily added

--- classes/_classes.php
+++ classes/_classes.php
@@ -3,6 +3,7 @@ require_once "TkSsoFrontEndCache.php";
 require_once "TkSsoBroker.php";
 require_once "TkDrupalSsoBroker.php";
 require_once "TkUsSsoBroker.php";
+require_once "TkSsoCustomRole.php";
 require_once "TkSsoRoleManager.php";
 require_once "TkSsoUser.php";
 require_once "TkSsoUtil.php";
