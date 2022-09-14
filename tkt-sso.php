e093821d9f49004cf9c341d7e0073a79af872c27
Mohamed Audi <78728931+mohamed-audi-thekey-technology@users.noreply.github.com>
2022-09-14 16:48:07 +0200

    Added isActiveUser functionality

--- tkt-sso.php
+++ tkt-sso.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TKT SSO
 Plugin URI:  https://www.thekey.technology
-Version:     77
+Version:     78
 Author:      the key technology
 Author URI:  https://www.thekey.technology
 License:     proprietary
