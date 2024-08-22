c6f046b5cd84da742014aeb39ea30f72775fca85
Mohamed Audi <78728931+mohamed-audi-thekey-technology@users.noreply.github.com>
2024-08-22 16:13:12 +0200

    Fixed  value

--- tkt-sso.php
+++ tkt-sso.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: TKT SSO
 Plugin URI:  https://www.thekey.technology
-Version:     91.0.1
+Version:     91.0.2
 Author:      the key technology
 Author URI:  https://www.thekey.technology
 License:     proprietary
